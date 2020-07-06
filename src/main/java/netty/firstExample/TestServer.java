b6f3ef32299f087f9cc044196fd05ce918c0dfd1
Shengchao Wu <wsc19890110@gmail.com>
2020-07-07 00:33:02 +0800

    init commit

---
+++ src/main/java/netty/firstExample/TestServer.java
@@ -0,0 +1,19 @@
+package netty.firstExample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TestServer {
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(null);
+
+    }
+}
---
+++ src/main/java/netty/firstExample/TestServerInitializer.java
@@ -0,0 +1,11 @@
+package netty.firstExample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+
+    }
+}
